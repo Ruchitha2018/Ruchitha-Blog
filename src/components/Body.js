7f0f8c5a17778d5a4496011b24b4ceb46cb49dad
Ruchitha2018 <ruchitadeshpande2023@gmail.com>
2023-11-29 11:07:06 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import Header from "./common/Header";
 
 
@@ -6,6 +6,7 @@ const Body = () => {
  return (
   <>
    <Header />
+   <h1><Link to="/tag-input-field">React Coding Questions</Link></h1>
    <Outlet />
   </>
  )
