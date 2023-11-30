b4538620cdd921c0d188281cda0aea0cc1c02492
Ruchitha2018 <ruchitadeshpande2023@gmail.com>
2023-11-30 18:37:24 +0530

    Redux Toolkit

--- src/components/ReduxToolkit.js
+++ src/components/ReduxToolkit.js
@@ -1,7 +1,117 @@
 import { Link } from "react-router-dom"
 import AddTodo from "./todos/AddTodo"
+import JSCode from "./common/JSCode"
 
 const ReduxToolkit = () => {
+
+  const code1 = `
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from './features/todos/todoSlice'
+
+export const store = configureStore({
+    reducer: todoReducer
+})
+  `
+
+  const code2 = `
+  import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+  const initialState = {
+      todos: []
+  }
+  
+  const todoSlice = createSlice({
+      name: 'todo',
+      initialState,
+      reducers: {
+          addTodo: (state, action) => {
+              const data = {
+                  id: nanoid(),
+                  text: action.payload
+              }
+  
+              state.todos.push(data)
+          },
+          removeTodo: (state, action) => {
+              state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+          }
+  
+      }
+  })
+  export const { addTodo, removeTodo } = todoSlice.actions
+  export default todoSlice.reducer;
+  `
+
+  const code3 = `
+  import { useState } from "react";
+import { addTodo, removeTodo } from "../../redux/features/todos/todoSlice";
+import { useDispatch, useSelector } from "react-redux";
+import './style.css'
+
+const AddTodo = () => {
+
+ const [input, setInput] = useState('');
+
+ const dispatch = useDispatch();
+ const todosData = useSelector((state) => state.todos)
+ const addTodoHandler = () => {
+  setInput('')
+  dispatch(addTodo(input));
+ }
+
+ const removeTodoHandler = (id) => {
+  dispatch(removeTodo(id))
+ }
+
+ return (
+  <>
+   <div>
+    <div className="todo-input-control py-3">
+     <input type="text" className="form-control" value={input} onChange={(e) => setInput(e.target.value)} />
+     <button className="btn btn-primary" onClick={addTodoHandler}>Add Todo</button>
+    </div>
+    <div className="todo-list">
+     <ul className="list-group">
+      {todosData.map((todo, index) => (
+       <li className="list-group-item">
+        <span>{todo.text}</span>
+        <span className="text-danger cursor-pointer" onClick={() => removeTodoHandler(todo.id)}>REMOVE</span>
+       </li>
+      ))}
+     </ul>
+    </div>
+   </div>
+  </>
+
+ )
+}
+
+export default AddTodo;
+  `
+const code4 = `
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+      <App />
+  </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+`  
   return (
     <>
       <div className="container py-3">
@@ -12,39 +122,21 @@ const ReduxToolkit = () => {
         </div>
         <div className="row align-items-center justify-content-center">
           <div className="col-lg-12">
-            <h1>Redux Toolkit</h1>
             <div>
-              <div>
-                <h5>Store</h5>
-                <ul>
-                  <li>The store is a single Javascript object that represents the entire state of the application.</li>
-                  <li>It is the central part of the Redux application. </li>
-                  <li>The store holds the current state, allows to dispatch actions to modify the state, and provides a way to subscribe to changes in the state.
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h5>Reducers</h5>
-                <ul>
-                  <li>A reducer is a pure function that specifies how the state should change in response to dispatched actions.</li>
-                  <li>Reducers take the current state and an action as arguments, and they return a new state based on the type of action.</li>
-                  <li>The key feature of a reducer is that it should not mutate the existing state but instead return a new state object.</li>
-                </ul>
-              </div>
-              <div>
-                <h5>createSlice</h5>
-                <p>createSlice from Redux Toolkit allows you to define both your reducer logic and action creators in a more concise manner.</p>
-              </div>
-              <div>
-                <h5>Reducers</h5>
-                <p>An object where the keys are action names, and the values are reducer functions.</p>
-              </div>
-              <div>
-                <h5>useSelector</h5>
-              </div>
-              <div>
-                <h5>useDispatch</h5>
-              </div>
+              <h6>1. Create store.js in src folder</h6>
+              <JSCode code={code1} />
+            </div>
+            <div>
+              <h6>2. Create todoSlice.js inside 'src/redux/features/todos' </h6>
+              <JSCode code={code2} />
+            </div>
+            <div>
+              <h6>3. Create AddTodo.js inside 'src/components/todos' </h6>
+              <JSCode code={code3} />
+            </div>
+            <div>
+              <h6>4. Add Provider inside index.js</h6>
+              <JSCode code={code4} />
             </div>
           </div>
         </div>
